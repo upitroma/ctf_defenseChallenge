a2d66c9f029a7d525c5c6696ad0539d544f84179
upitroma <55750919+upitroma@users.noreply.github.com>
2022-07-04 21:13:10 -0500

    idk

--- main.sh
+++ main.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# increment port number
 INCREMENT=$(cat increment.txt)
 echo $INCREMENT
 echo -n $((INCREMENT + 1)) > increment.txt
+
+
+echo "Starting container..."
+docker run -d -p $INCREMENT:22 --name $INCREMENT ctf 
+
+# handle suddon stop
+function cleanup {
+    echo "Deleting container..."
+    docker rm -f $INCREMENT
+}
+trap cleanup EXIT
+
+echo "container running on port $INCREMENT"
+read 
